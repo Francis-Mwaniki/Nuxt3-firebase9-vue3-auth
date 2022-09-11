bb3f562ee67dd4c30eae754e8faba9e3d65c1437
Francis-Mwaniki <francismwaniki630@gmail.com>
2022-09-11 11:19:24 +0300

    Nuxt3-firebase Auth

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+publicRuntimeConfig:{
+FIREBASE_API_KEY:process.env.FIREBASE_API_KEY,
+},
+privateRuntimeConfig:{
+    FIREBASE_API_KEY:process.env.FIREBASE_API_KEY,
+}
+})
